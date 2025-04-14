704a13f944d1d8a6e606eea0590de0c6808f9a54
resianlucas <rm95781@fiap.com.br>
2025-04-14 07:57:45 -0300

    implementando o banner e um pequeno cabecalho

---
+++ src/components/Banner/index.js
@@ -0,0 +1,31 @@
+import styles from './Banner.module.css';
+import foto from 'assets/minha_foto.png';
+import circulo from 'assets/circulo_colorido.png';
+
+export default function Banner() {
+    return (
+        <div className={styles.banner}>
+            <div className={styles.apresentacao}>
+                <h1 className={styles.titulo}>
+                    Olá, mundo!
+                </h1>
+                <p className={styles.paragrafo}>
+                    Sou um desenvolvedor web em início de carreira, com foco em tecnologias modernas como HTML, CSS, JavaScript e React. Estou em constante aprendizado e busco criar interfaces limpas, funcionais e com uma boa experiência para o usuário. Tenho grande interesse em desenvolvimento front-end e estou construindo meu portfólio para atuar como freelancer e evoluir cada vez mais na área.
+                </p>
+            </div>
+
+            <div className={styles.imagens}>
+                <img
+                    className={styles.circuloColorido}
+                    src={circulo}
+                />
+                <img
+                    className={styles.minhaFoto}
+                    src={foto}
+                    alt='minha foto'
+                />
+
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Menu/index.js
@@ -0,0 +1,11 @@
+import styles from './Menu.module.css';
+
+export default function Menu() {
+    return (
+        <header>
+            <nav className={styles.navegacao}>
+                Lucas Antunes | Desenvolvedor Web
+            </nav>
+        </header>
+    )
+}
